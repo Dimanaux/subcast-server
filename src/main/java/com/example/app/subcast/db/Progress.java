0ee05c91b5f04ff3029b3a5036c08080d82094d2
cosmoskey <barskoidim@gmail.com>
2019-04-14 16:44:56 +0300

    Add composite key for Progress. account.id + episode.guid

--- src/main/java/com/example/app/subcast/db/Progress.java
+++ src/main/java/com/example/app/subcast/db/Progress.java
@@ -1,40 +1,29 @@
 package com.example.app.subcast.db;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 /**
  * represents someone's progress on particular podcast episode
  */
 @Entity
 @Table(name = "progress")
+@IdClass(ProgressCompositeKey.class)
 public class Progress {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
-
     // whose progress (account identifier)
+    @Id
     @Column(name = "account_id")
     private Long accountId;
 
-    @ManyToOne
-    @JoinColumn(name = "episode_guid")
-    private Episode episode;
+    @Id
+    @Column(name = "episode_guid")
+    private String episodeGuid;
 
     // time listened in seconds
     @Column(name = "time")
     private Integer time;
 
     public Progress() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Progress setId(Long id) {
-        this.id = id;
-        return this;
     }
 
     public Long getAccountId() {
@@ -46,12 +35,12 @@ public class Progress {
         return this;
     }
 
-    public Episode getEpisode() {
-        return episode;
+    public String getEpisodeGuid() {
+        return episodeGuid;
     }
 
-    public Progress setEpisode(Episode episode) {
-        this.episode = episode;
+    public Progress setEpisodeGuid(String episodeGuid) {
+        this.episodeGuid = episodeGuid;
         return this;
     }
 
@@ -61,6 +50,38 @@ public class Progress {
 
     public Progress setTime(Integer time) {
         this.time = time;
+        return this;
+    }
+}
+
+@Embeddable
+class ProgressCompositeKey implements Serializable {
+    private Long accountId;
+    private String episodeGuid;
+
+    public ProgressCompositeKey() {
+    }
+
+    public ProgressCompositeKey(Long accountId, String episodeGuid) {
+        this.accountId = accountId;
+        this.episodeGuid = episodeGuid;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public ProgressCompositeKey setAccountId(Long accountId) {
+        this.accountId = accountId;
+        return this;
+    }
+
+    public String getEpisodeGuid() {
+        return episodeGuid;
+    }
+
+    public ProgressCompositeKey setEpisodeGuid(String episodeGuid) {
+        this.episodeGuid = episodeGuid;
         return this;
     }
 }
